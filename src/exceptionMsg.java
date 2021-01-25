b93154b3d08126827578d1bd6b495e1404324c20
Cristian <cristian.c3.fl@gmail.com>
2021-01-25 01:12:02 -0500

    Add examples of exeptions

--- src/exceptionMsg.java
+++ src/exceptionMsg.java
@@ -1,4 +1,53 @@
 package src;
 
+import java.io.*;
+
 public class exceptionMsg {
+
+    public String exceptionTryCatch(){
+        String sNull= null;
+        String sIndexOut = "";
+        //To add try and catch, you can go to the menu, CODE --> Surround it or (Cntrl - Alt - T)
+        try {
+            //It going to show a error because s is null, and it not have substring.
+            var sub = sNull.substring(1);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            System.out.println("Null pointer..");
+        }
+        try {
+            var sub1 = sIndexOut.substring(1);
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+            System.out.println("Index out bounds..");
+        }
+        return sNull + sIndexOut;
+    }
+
+/*  It is possible add more than one catch in one try
+   try {
+                var test = t.substring(1);
+    } catch (NullPointerException e) {
+        e.printStackTrace();
+        System.out.println("Null pointer..");
+    } catch (IndexOutOfBoundsException e) {
+        e.printStackTrace();
+        System.out.println("Index out bounds..");
+    }*/
+    //ALSO IT IS POSSIBLE ADD ARG TO THE TRY
+    //This help to close the objets and not use memory of the system.
+    public String fileExeption(String path)
+    {
+        var text = "";
+        try (FileReader reader = new FileReader(path);
+             BufferedReader br = new BufferedReader(reader))
+        {
+            text = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return text;
+    }
+
 }
